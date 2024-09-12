407c0069bb1908bb143879ae5a6228d09e121fbf
baksabence <gxdcloud@gmail.com>
2024-09-12 20:32:53 +0200

    teszt

--- Jatekter.js
+++ Jatekter.js
@@ -9,6 +9,7 @@ export default class Jatekter{
         this.kisKepMegjelenit()
     }
 
+    
     kisKepMegjelenit(){
         this.#kepekAdatai.forEach(adat =>{
             this.#szuloElem.append(`<div class="col-12 col-sm-6 col-md-4 col-lg-3"> <img src="${adat.kep}" class="img-fluid"> </div>`)
